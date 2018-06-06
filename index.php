e03a71504f05cff5f6ee697811e77526760b39f3
Lital Josifov <litalj@wobi.co.il>
2018-06-06 16:08:10 +0300

    Proccess

--- index.php
+++ index.php
@@ -1,15 +1,60 @@
 <?php
-
-$url = 'https://www.geektime.co.il/angularup-2018/';
+$url = $argv[0];
+// echo $url;
+// $url = 'https://www.geektime.co.il/angularup-2018/';
 $directory = 'C:\xampp\htdocs\image-table\\';
-/*
+
 $html = file_get_contents($url);
 $pattern = '<img\s+src="([^"]+)"\s*+>';
 preg_match_all($pattern, $html, $matches);
-*/
 
+$bootstrap_css_file = '<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">';
+$html_to_file = '<html><head>'.$bootstrap_css_file.'</head><body><div class="col-md-8 col-md-offset-2">';
+
+$html_table = '<table class="table"><tr><th>Image</th><th>Image Details</th></tr>';
+
+foreach($matches[1] as $imageUrl) {
+  $imageData = parse_url($imageUrl);
+  $imageInfo = pathinfo($imageData['path']); // , PATHINFO_EXTENSION
+  // pa($imageData);
+  $isImageUrl = array_key_exists('scheme', $imageData) && array_key_exists('host', $imageData) && array_key_exists('path', $imageData);
+  echo $isImageUrl;
+  // if ($isImageUrl) break;
+  // $imgLink = $imageData['scheme'] . '://'. $imageData['host'] . $imageData['path'];
+  // echo $imgLink;
+
+  // $filename = basename($imgLink);
+  // $complete_save_loc = $directory . $filename;
+  // $html_table = $html_table.'<tr><td>'.img_details($imageInfo['extension'], getimagesize($imgLink)[3], $imgLink).'</td><td>'.imgElement($imgLink).'</td></tr>';
+  // file_put_contents($complete_save_loc, file_get_contents($imgLink));
+}
+
+$html_table = $html_table.'</table>';
+$html_to_file = $html_to_file.$html_table.'</div></body></html>';
+file_put_contents('index.html', $html_to_file);
+
+function img_details($extension, $size, $url) {
+  return '<div>
+  <h4>Image URL: '.$url.'</h4>
+  <h4>Image format: '.$extension.'</h4>
+  <h4>Image size: '.$size.'</h4>
+  </div>';
+}
+
+function imgElement($src_url) {
+  return '<img src="'.$src_url.'" style="max-width: 120px;">';
+}
+
+
+function pa($a) {
+  echo "<pre>";
+  print_r($a);
+  echo "</pre>";
+}
 /*
-*/
+
+// pa($imageUrl);
+// pa($imageInfo);
 $matches = array (
     0 => 
     array (
@@ -26,27 +71,5 @@ $matches = array (
       3 => 'https://www.geektime.co.il/wp-content/themes/geektime/css/images/geek.png?x35493',
     ),
 );
-echo '<table>';
-echo '<tr><th>Image</th><th>Image Details</th></tr>';
-echo '<tr><td></td><td></td></tr>';
-echo '</table>';
-foreach($matches[1] as $imageUrl) {
-    // pa("start");
-    // pa($imageUrl);
-    $imageeData = parse_url($imageUrl);
-    // pa($imageeData);
-    // $imageInfo = pathinfo($imageeData['path']); // , PATHINFO_EXTENSION
-    // pa($imageInfo);
-    $imgLink = $imageeData['scheme'] . '://' . $imageeData['host'] . $imageeData['path'];
-    $filename = basename($imgLink);
-    $complete_save_loc = $directory . $filename;
-    file_put_contents($complete_save_loc, file_get_contents($imgLink));
-    echo '<br> img saved: ' . $imgLink;
-}
-
-function pa($a) {
-    echo "<pre>";
-    print_r($a);
-    echo "</pre>";
-}
-
+  
+*/
