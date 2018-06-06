fc1c884803c5a10da24e91956e5fb938c49b9d22
Lital <lital.jos@gmail.com>
2018-06-06 22:09:24 +0300

    Finished

--- index.php
+++ index.php
@@ -1,39 +1,82 @@
 <?php
-$url = $argv[0];
-// echo $url;
-// $url = 'https://www.geektime.co.il/angularup-2018/';
-$directory = 'C:\xampp\htdocs\image-table\\';
 
-$html = file_get_contents($url);
+// validate required arguments
+if(!isset($argv[1])) {
+  exit("Please provide url as argument 1");
+}
+
+if(!isset($argv[2])) {
+  exit("Please provide folder path as argument 2");
+}
+
+$url = $argv[1];
+$directory = rtrim($argv[2], DIRECTORY_SEPARATOR).DIRECTORY_SEPARATOR;
+$htmlToFile = '';
+
+// Check if it's realy diretcory
+if(!is_dir($directory)) {
+  exit("Directory is not exist: $directory");
+}
+
+// fetch url html
+$curl = curl_init($url);
+curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1); 
+$result = curl_exec($curl);
+if ($result === false) {
+  exit("Can't fetch url: $url");
+} 
+
+// $url = 'https://www.geektime.co.il/angularup-2018/';
+// $directory = 'C:\xampp\htdocs\image-table\\';
+
+$html = $result;
+
 $pattern = '<img\s+src="([^"]+)"\s*+>';
 preg_match_all($pattern, $html, $matches);
 
-$bootstrap_css_file = '<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">';
-$html_to_file = '<html><head>'.$bootstrap_css_file.'</head><body><div class="col-md-8 col-md-offset-2">';
 
-$html_table = '<table class="table"><tr><th>Image</th><th>Image Details</th></tr>';
+$htmlToFile .= '<table class="table"><tr><th>Image</th><th>Image Details</th></tr>';
 
 foreach($matches[1] as $imageUrl) {
   $imageData = parse_url($imageUrl);
-  $imageInfo = pathinfo($imageData['path']); // , PATHINFO_EXTENSION
-  // pa($imageData);
+  $imageInfo = pathinfo($imageData['path']);
   $isImageUrl = array_key_exists('scheme', $imageData) && array_key_exists('host', $imageData) && array_key_exists('path', $imageData);
-  echo $isImageUrl;
-  // if ($isImageUrl) break;
-  // $imgLink = $imageData['scheme'] . '://'. $imageData['host'] . $imageData['path'];
-  // echo $imgLink;
-
-  // $filename = basename($imgLink);
-  // $complete_save_loc = $directory . $filename;
-  // $html_table = $html_table.'<tr><td>'.img_details($imageInfo['extension'], getimagesize($imgLink)[3], $imgLink).'</td><td>'.imgElement($imgLink).'</td></tr>';
-  // file_put_contents($complete_save_loc, file_get_contents($imgLink));
+  if (!$isImageUrl) {
+    continue;
+  }
+  $imgLink = $imageData['scheme'] . '://'. $imageData['host'] . $imageData['path'];
+  
+  $filename = basename($imgLink);
+  $completeSaveLoc = $directory . $filename;
+  $htmlToFile .= '<tr><td>'.imgDetails($imageInfo['extension'], getimagesize($imgLink)[3], $imgLink).'</td><td>'.imgElement($imgLink).'</td></tr>';
+  file_put_contents($completeSaveLoc, file_get_contents($imgLink));
 }
 
-$html_table = $html_table.'</table>';
-$html_to_file = $html_to_file.$html_table.'</div></body></html>';
-file_put_contents('index.html', $html_to_file);
+$htmlToFile .= '</table>';
 
-function img_details($extension, $size, $url) {
+saveContent($htmlToFile, $directory);
+
+
+function saveContent($content = "", $directory = "", $filename = "index.html") {
+  $html = buildHtml($content);
+  file_put_contents($directory.$filename, $html);
+}
+
+function buildHtml($content = "") {
+  return '<html>
+            <head>
+              <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+            </head>
+            <body>
+              <div class="col-md-8 col-md-offset-2">
+                '. $content .'
+              </div>
+            </body>
+          </html>
+          ';
+}
+
+function imgDetails($extension, $size, $url) {
   return '<div>
   <h4>Image URL: '.$url.'</h4>
   <h4>Image format: '.$extension.'</h4>
@@ -41,35 +84,6 @@ function img_details($extension, $size, $url) {
   </div>';
 }
 
-function imgElement($src_url) {
-  return '<img src="'.$src_url.'" style="max-width: 120px;">';
+function imgElement($srcUrl) {
+  return '<img src="'.$srcUrl.'" style="max-width: 120px;">';
 }
-
-
-function pa($a) {
-  echo "<pre>";
-  print_r($a);
-  echo "</pre>";
-}
-/*
-
-// pa($imageUrl);
-// pa($imageInfo);
-$matches = array (
-    0 => 
-    array (
-      0 => 'img src="https://www.geektime.co.il/wp-content/themes/geektime/css/images/mobile-menu-icon.svg?x35493" ',
-      1 => 'img src="https://www.geektime.co.il/wp-content/themes/geektime/css/images/logo.png?x35493" ',
-      2 => 'img src="https://www.geektime.co.il/wp-content/themes/geektime/css/images/loading.gif?x35493" ',
-      3 => 'img src="https://www.geektime.co.il/wp-content/themes/geektime/css/images/geek.png?x35493" ',
-    ),
-    1 => 
-    array (
-      0 => 'https://www.geektime.co.il/wp-content/themes/geektime/css/images/mobile-menu-icon.svg?x35493',
-      1 => 'https://www.geektime.co.il/wp-content/themes/geektime/css/images/logo.png?x35493',
-      2 => 'https://www.geektime.co.il/wp-content/themes/geektime/css/images/loading.gif?x35493',
-      3 => 'https://www.geektime.co.il/wp-content/themes/geektime/css/images/geek.png?x35493',
-    ),
-);
-  
-*/
